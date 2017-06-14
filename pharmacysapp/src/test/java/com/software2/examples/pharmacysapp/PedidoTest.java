0c70fef4ea83712807aade1bcc2f53bec785df2a
Cliente CONTROL-PC <Cliente CONTROL-PC>
2017-06-14 15:32:09 -0500

    Add Test 4

--- pharmacysapp/src/test/java/com/software2/examples/pharmacysapp/PedidoTest.java
+++ pharmacysapp/src/test/java/com/software2/examples/pharmacysapp/PedidoTest.java
@@ -124,5 +124,28 @@ public class PedidoTest {
         System.out.println("----Test 3----\n");      
     }
     
-
+    @Test  
+    public void testIntegracion_PagoSubtotalHorario() {
+        System.out.println("----Test 4----");      
+        Producto p1 = obtener_producto_catalogo(catalogo,"Analgan");
+        Producto p2 = obtener_producto_catalogo(catalogo,"Diclofenaco");
+        DetallePedido detalle1 = new DetallePedido(p1,1);
+        DetallePedido detalle2 = new DetallePedido(p2,1);
+        carrito.add(detalle1);
+        carrito.add(detalle2);
+        //visualizar el pedido con los productos seleccionados.
+        System.out.println(carrito.toString());
+        Pago pa=new Pago();
+        //Ingresa el tipo de pago
+        pa.crear_pago(true,"");
+        Cliente client=new Cliente("Kerly", 2, pa);
+        System.out.println(client.InfoPer());
+        System.out.println( pa.validar_pago(pa));
+        double subt=detalle1.subtotal +detalle2.subtotal;
+        System.out.println("El subtotal a pagar es: " + subt);
+        Pedido pedido = new Pedido(carrito, new Date(), client);
+        assertEquals("Horario disponible", pedido.ValidHora());//experado,obtenido
+        System.out.println("----Test 4----\n");  
+    }
+    
 }
