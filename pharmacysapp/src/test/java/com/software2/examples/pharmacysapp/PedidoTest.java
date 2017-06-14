d98e2508aa858c41fc13bed360fb2c41e66d5970
Cliente CONTROL-PC <Cliente CONTROL-PC>
2017-06-14 16:24:26 -0500

    test 5

--- pharmacysapp/src/test/java/com/software2/examples/pharmacysapp/PedidoTest.java
+++ pharmacysapp/src/test/java/com/software2/examples/pharmacysapp/PedidoTest.java
@@ -129,7 +129,7 @@ public class PedidoTest {
     public void testIntegracion_confirmarPagoHorario() {
         System.out.println("----Test 4----");      
         Producto p1 = obtener_producto_catalogo(catalogo,"Analgan");
-        Producto p2 = obtener_producto_catalogo(catalogo,"Diclofenaco");
+        Producto p2 = obtener_producto_catalogo(catalogo,"Dicloflenaco");
         DetallePedido detalle1 = new DetallePedido(p1,1);
         DetallePedido detalle2 = new DetallePedido(p2,1);
         carrito.add(detalle1);
@@ -147,5 +147,31 @@ public class PedidoTest {
         Pedido pedido=new Pedido(carrito,"09:30",client);
         assertEquals("Horario disponible", pedido.ValidHora();
         System.out.println("----Test 4----\n");      
+    }
+
+    @Test  
+    public void testIntegracion_confirmarRecaTotal() {
+        System.out.println("----Test 5----");      
+        Producto p1 = obtener_producto_catalogo(catalogo,"Analgan");
+        Producto p2 = obtener_producto_catalogo(catalogo,"Dicloflenaco");
+        DetallePedido detalle1 = new DetallePedido(p1,1);
+        DetallePedido detalle2 = new DetallePedido(p2,1);
+        carrito.add(detalle1);
+        carrito.add(detalle2);
+        //visualizar el pedido con los productos seleccionados.
+        System.out.println(carrito.toString());
+        Pago pa=new Pago();
+        //Ingresa el tipo de pago
+        pa.crear_pago(true,"");
+        Cliente client=new Cliente("Kerly", 2, pa);
+        System.out.println(client.InfoPer());
+        System.out.println( pa.validar_pago(pa));
+        double subt=detalle1.subtotal +detalle2.subtotal;
+        System.out.println("El subtotal a pagar es: " + subt);
+        double recargo=pedido.GetRecargo(client,  subt);
+        double total=recargo+subt;
+        Pedido pedido=new Pedido(carrito,"09:30",client);
+        assertEquals(total, pedido.TotalPedido());
+        System.out.println("----Test 5----\n");      
     }
 }
